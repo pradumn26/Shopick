2d3765465866c260071822575441d2960badd45f
pradumn26 <pradumn.26@gmail.com>
2018-01-11 16:24:22 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.resolve('./public')));
 app.get('/', (req, res) => {
-    res.sendFile('homepage.html');
+    res.sendFile(path.resolve('./homepage.html'));
 });
 
 const PORT = process.env.PORT || 5000;
